b19b6a7d9a38cd63cf9890b4fea444fcd7032dcb
ben4235 <bpiatkowski02@gmail.com>
2025-02-27 18:06:11 -0500

    this is clock part 2. the clock added a second hand which moves the hour and the minute hand. the minute is 12 times faster than the hour and they both change based on the slider

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    [Range(0, 1)]
-    public float speed = 1;
+    [Range(0, 0.02f)]
+    public float hour_speed;
+    public float minute_speed;
+
+    public Transform Hour_Hand;  // Reference to the hour hand object
+    public Transform Minute_Hand;  // Reference to the minute hand object
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +20,13 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Set the minute speed based on hour speed at the start
+        minute_speed = hour_speed * 12;
+
+
         Vector3 point = new Vector3(0, 0, 0); // The point around which the object will rotate
-        transform.RotateAround(point, Vector3.forward, -speed); // Rotate around the point on the z-axis
+        Hour_Hand.RotateAround(point, Vector3.forward, -hour_speed); // Rotate around the point on the z-axis
+        Minute_Hand.RotateAround(point, Vector3.forward, -minute_speed); // Rotate around the point on the z-axis
+
     }
 }
