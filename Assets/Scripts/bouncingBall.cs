595ab8aa3229f5d57368365eec7e77217ac6dcfb
Benjamin Piatkowski <bpiatkowski02@gmail.com>
2025-03-10 12:26:27 -0400

    bouncing ball script changed to be completed
    added the space and arrow key functions

--- Assets/Scripts/bouncingBall.cs
+++ Assets/Scripts/bouncingBall.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 public class bouncingBall : MonoBehaviour
 {
@@ -31,6 +32,34 @@ public class bouncingBall : MonoBehaviour
             speedY = speedY * -1;
         }
         transform.position = pos;
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            transform.localScale *= 1.2f;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.localScale *= 0.8f;
+        }
+        if( Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            speedX *= 1.2f;
+            speedY *= 1.2f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            speedX *= 0.8f;
+            speedY *= 0.8f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = new Vector3(0, 0, 0);
+
+                speedY = Random.Range(-1f, 1f); // Random float between -1 and 1
+                speedX = Random.Range(-1f, 1f); // Random float between -1 and 1
+
+        }
 
     }
 }
